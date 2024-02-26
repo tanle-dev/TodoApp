d5100e1c347fcc4483ee0640f23d0820b82bb244
tanle <lenhuttan.programing@gmail.com>
2024-02-26 17:06:16 -0500

    Add feature Modal

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -6,7 +6,7 @@ export function Header(){
     return (
         <View style = {styles.container}>
             <View style = {styles.logoContainer}>
-                <FontAwesome name="tasks" size={36} color={'#FFF'}/>
+                <FontAwesome name="gitlab" size={36} color={'#FFF'}/>
                 <Text style={styles.logoText}>TODO LIST</Text>
             </View>
             <Text style={styles.authorName}>by Tan Le</Text>
--- src/components/Tasks/Task/Task.js
+++ src/components/Tasks/Task/Task.js
@@ -1,13 +1,35 @@
-import { Text, View } from "react-native";
+import { AntDesign, FontAwesome } from "@expo/vector-icons";
+import { Modal, Pressable, Switch, Text, View } from "react-native";
 import { styles } from "./styles";
 
 export function Task(props){
     const {task} = props
     return(
-        <View style={styles.container}>
-            <Text style={styles.title}>{task.title}</Text>
-            <Text style={styles.id}>Id: {task.id}</Text>
-            <Text style={styles.id}>Status: {task.status ? (<Text style={{color:"green"}}>Completed</Text>): (<Text style={{color:"red"}}>Opening</Text>)}</Text>
-        </View>
+        <Pressable>
+            <View style={styles.container}>
+                <Text style={styles.title}>{task.title}</Text>
+                <Text style={styles.id}>Id: {task.id}</Text>
+                <Text style={styles.id}>Status: {task.status ? (<Text style={{color:"green"}}>Completed</Text>): (<Text style={{color:"red"}}>Opening</Text>)}</Text>
+            </View>
+            <Modal>
+                <View>
+                    <View>
+                        <FontAwesome name='close'></FontAwesome>
+                        <Text>Close</Text>
+                    </View>
+                    <Text style={styles.title}>{task.title}</Text>
+                    <View>
+                        <View>
+                            <Switch></Switch>
+                            <Text style={{color:"green"}}>Completed</Text>
+                        </View>
+                        <View>
+                            <AntDesign name='delete' style={{color:"red"}} size={40}></AntDesign>
+                            <Text style={{color:"red"}}>Remove</Text>
+                        </View>
+                    </View>
+                </View>
+            </Modal>
+        </Pressable>
     )
 }
